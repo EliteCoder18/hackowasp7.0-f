3c14b4784812f72eb2171f4d248596119bff5dc3
Harkirat Singh <singharkirath1511@gmail.com>
2025-04-19 15:43:28 +0530

    frontend initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div className='bg-black text-white p-10 text-5xl'>Hi there!</div>
+    </>
+  )
+}
+
+export default App
